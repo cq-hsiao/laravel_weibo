31995cc4ada3b93b76287745f232a23ded66df6a
cq-hsiao <124328227@qq.com>
2019-07-01 06:50:04 +0000

    Updated database configuration

---
+++ app/helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 不同环境的数据库连接方式
+ * @return array
+ */
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
