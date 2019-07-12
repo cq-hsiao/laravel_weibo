8c8464125e367c109996a223ef6e029f880843d6
cq-hsiao <124328227@qq.com>
2019-07-12 08:05:34 +0000

    用户激活

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,8 +18,8 @@ class UsersTableSeeder extends Seeder
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = 'Hsiao';
+        $user->email = 'hsiao@example.com';
         $user->is_admin = true;
         $user->save();
     }
