b86cc7fff5af9b9aaaed621101381bcfa17e93cf
cq-hsiao <124328227@qq.com>
2019-07-12 09:23:31 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,8 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    public function statuses(){
+        $this->hasMany(Status::class);
+    }
 }
