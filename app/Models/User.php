f97afec1bab462d53098639dc72da6f75787f71d
cq-hsiao <124328227@qq.com>
2019-08-05 06:52:30 +0000

    关注按钮

--- app/Models/User.php
+++ app/Models/User.php
@@ -92,5 +92,6 @@ class User extends Authenticatable
     public function isFollowing($user_id)
     {
         return $this->followings->contains($user_id);
+        //return $this->>followings()->get()->contains($user_id);
     }
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -28,4 +28,11 @@ class UserPolicy
     {
         return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
+
+    public function follow(User $currentUser,User $user)
+    {
+        return $currentUser->id !== $user->id;
+    }
+
+
 }
